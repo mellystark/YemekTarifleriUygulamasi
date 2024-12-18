3a8024812859e06395485b3e8b57f1eb2f7dd203
melly <kurabiyecanavari773@gmail.com>
2024-12-18 16:51:43 +0300

    giriş taslağı oluşturuldu

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YemekTarifleriUygulamasi.Data;
+using YemekTarifleriUygulamasi.Models;
+using BCrypt.Net;
+
+namespace YemekTarifleriUygulamasi.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                // Hash the password before saving
+                user.Password = BCrypt.Net.BCrypt.HashPassword(user.Password);
+
+                _context.Users.Add(user);
+                _context.SaveChanges();
+
+                return RedirectToAction("Login");
+            }
+
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            // Kullanıcıyı veritabanından al
+            var user = _context.Users.FirstOrDefault(u => u.Username == username);
+
+            if (user != null)
+            {
+                // Şifreyi kontrol et (hash karşılaştırması)
+                if (BCrypt.Net.BCrypt.Verify(password, user.Password))
+                {
+                    // Başarılı giriş
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    // Şifre hatalı
+                    ViewBag.ErrorMessage = "Invalid password.";
+                    return View();
+                }
+            }
+            else
+            {
+                // Kullanıcı bulunamadı
+                ViewBag.ErrorMessage = "User not found.";
+                return View();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using YemekTarifleriUygulamasi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+
+// Configure database connection
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 
 var app = builder.Build();
 
